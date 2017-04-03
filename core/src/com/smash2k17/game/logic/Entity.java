a0ce65f742aa19bf6068dce0d342c7aef32a3437
EvelienHermans <emhermans98@gmail.com>
2017-04-03 12:39:52 +0200

    Entity

--- core/src/com/smash2k17/game/logic/Entity.java
+++ core/src/com/smash2k17/game/logic/Entity.java
@@ -3,6 +3,7 @@ package com.smash2k17.game.logic;
 import com.smash2k17.game.logic.interfaces.IPlayable;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 /**
  * Created by BePul on 27-3-2017.
@@ -10,35 +11,45 @@ import java.awt.*;
 public abstract class Entity implements IPlayable{
 
     private String name;
-    private int hitPoints;
+    int hitPoints;
     private Point position;
     private int strength;
     private int armor;
+    private Map map;
 
-    public Entity(String name, int hitPoints, Point position, int strength, int armor)
+    public Entity(String name, int hitPoints, Point position, int strength, int armor, Map map)
     {
         this.name = name;
         this.hitPoints = 100;
         this.position = position;
         this.strength = 100;
         this.armor = 0;
+        this.map = map;
     }
-
-    public void Move(){
-
+    public void Move(KeyEvent e){
+        if(KeyEvent.getKeyText(e.getKeyCode()) == "a"){
+            //als position groter is als het einde van de map
+            if (position.x > 0){
+                position.x --;
+            }
+        }
+        if(KeyEvent.getKeyText(e.getKeyCode()) == "d"){
+            //als position kleiner is als het einde van de map
+            if(position.x < map.getGridWidth()){
+                position.x ++;
+            }
+        }
     }
 
     public void Jump(){
-
+        position.y += 20;
     }
 
     public void Attack(){
 
     }
 
-    public void Respawn(){
-        if(hitPoints <= 0){
-            hitPoints = 100;
-        }
-    }
+    public void Respawn(){}
+
+
 }
